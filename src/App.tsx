7bb5acc101101853db83de9c03a363d7c436adc8
gallodev <contato.gallodev@gmail.com>
2023-03-23 18:09:38 -0300

    feat: Menu lateral

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+/* eslint-disable linebreak-style */
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
 import { AppThemeProvider } from './shared/context/ThemeContext';
+import { MenuLateral } from './shared/components';
 
 export const App: React.FC = () => {
   return (
-      <AppThemeProvider>
-        <BrowserRouter>
-          <AppRoutes/>
-        </BrowserRouter>
-      </AppThemeProvider>
+    <AppThemeProvider>
+      <BrowserRouter>
+        <MenuLateral>
+          <AppRoutes/>        
+        </MenuLateral>
+      </BrowserRouter>
+    </AppThemeProvider>
   );
-}
+};
---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,36 @@
+import { Box, Drawer, useTheme, Avatar, Divider, List, ListItemButton, Icon, ListItemText, ListItemIcon } from '@mui/material';
+
+interface IMenuLateralProps {
+    children: React.ReactNode
+}
+
+export const MenuLateral : React.FC<IMenuLateralProps> = ({ children } : IMenuLateralProps) => {
+  const theme = useTheme();
+  return (
+    <>
+      <Drawer open={true} variant="permanent">
+        <Box width={theme.spacing(28)} display="flex" flexDirection="column" height="100%">
+          <Box width="100%" height={theme.spacing(20)} display="flex" justifyContent="center" alignItems="center">
+            <Avatar 
+              sx={{height: theme.spacing(12) , width: theme.spacing(12) }}
+              src="https://scontent.fssz3-1.fna.fbcdn.net/v/t1.6435-9/117445059_1334242233573453_9009871377713748541_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=b4OTobAxKo4AX9JhZpw&_nc_ht=scontent.fssz3-1.fna&oh=00_AfCtEhVbuCG1EbhxByzxZ7Pm_GSNxQVlsqp06zx-D-nHqg&oe=644440DD"/>
+          </Box>
+          <Divider/>
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Página inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>
+      </Drawer>
+      <Box height="100vh" marginLeft={theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  );
+};
