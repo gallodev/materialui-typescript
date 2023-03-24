beaa82ecb995a40cd0938120e1b9f040438f3bac
gallodev <contato.gallodev@gmail.com>
2023-03-23 21:07:02 -0300

    fix: eslint fix

--- src/shared/themes/dark.ts
+++ src/shared/themes/dark.ts
@@ -1,23 +1,23 @@
-import { createTheme } from '@mui/material'
+import { createTheme } from '@mui/material';
 import { cyan, yellow } from '@mui/material/colors';
 
 export const DarkTheme = createTheme({
-    palette: {
-        primary: {
-            main: yellow[700],
-            dark: yellow[800],
-            light: yellow[500],
-            contrastText: "#FFFFFF", 
-        },
-        secondary: {
-            main: cyan[700],
-            dark: cyan[800],
-            light: cyan[500],
-            contrastText: "#FFFFFF", 
-        },
-        background: {
-            default: "#303134",
-            paper: "#202124",
-        }
+  palette: {
+    primary: {
+      main: yellow[700],
+      dark: yellow[800],
+      light: yellow[500],
+      contrastText: '#FFFFFF', 
+    },
+    secondary: {
+      main: cyan[700],
+      dark: cyan[800],
+      light: cyan[500],
+      contrastText: '#FFFFFF', 
+    },
+    background: {
+      default: '#303134',
+      paper: '#202124',
     }
+  }
 });
