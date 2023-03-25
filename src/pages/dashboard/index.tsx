6310dbed319fff4f61fea40c344ec68169b3d205
gallodev <contato.gallodev@gmail.com>
2023-03-25 18:26:45 -0300

    feat: tollbar

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,9 @@
 import { LayoutBase } from '../../shared/layouts';
+import { BarraDeFerramentas } from '../../shared/components';
 
 export const Dashboard: React.FC = () => {
   return (
-    <LayoutBase title="Página inicial">
+    <LayoutBase title="Página inicial" toolbar={<BarraDeFerramentas isShowSearch/>}>
         testando
     </LayoutBase>
   );
